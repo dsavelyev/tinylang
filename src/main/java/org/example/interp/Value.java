957f8b85f4433ea693e71d59cc6689a4541f6985
Dmitry Savelyev <swedmitrysav@gmail.com>
2026-05-20 08:07:22 +0200

    make everything truthy by default

--- src/main/java/org/example/interp/Value.java
+++ src/main/java/org/example/interp/Value.java
@@ -2,7 +2,7 @@ package org.example.interp;
 
 public abstract class Value {
     public boolean toBoolOrThrow() {
-        throw new InterpreterError(InterpreterError.Kind.TYPE_MISMATCH, "type mismatch, expected boolean");
+        return true;
     }
 
     public int toIntOrThrow() {
